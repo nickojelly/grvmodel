5f38c8eb4a3b2bee5c0fe345f4b8a04f623f4a8b
nickojelly <72242022+nickojelly@users.noreply.github.com>
2022-04-10 17:38:13 +1000

    Significant changes to R feature gen and Input Layers
    R feature gen:
    
    Now holds polynomial function for mapping avg speed vs weigh and other factors
    
    Input Layer:
    
    Input Lay now holds new measures including standard dev, mean and max for all dogs.

--- Python/New Feature Creation/ipython_cell_input.py
+++ Python/New Feature Creation/ipython_cell_input.py
@@ -1,19 +1,33 @@
+
+import os
+import xml.etree.ElementTree as ET
+import pandas as pd
+from datetime import datetime
+import pickle
+import tqdm
+import line_profiler
+from operator import itemgetter
+
+def input_layer_gen(form):
+    race_forms = form.groupby(["race_id"])
+    for i,j in race_forms:
+        print(i,j)
+        break
+
+def track_id_gen(dist, track):
+    return track[0:4]+"-"+str(int(dist))
+
 #pickle Load results
-resultspicklefile = open( r'C:\Users\Nick\Documents\GitHub\grvmodel\Data Processed\results-df-w-dates.npy', 'rb')
+resultspicklefile = open( 'results-df.npy', 'rb')
 resultsdf = pickle.load(resultspicklefile)
-resultsdf = pd.DataFrame(resultsdf)
-resultsdf.head()
+resultsdf = pd.DataFrame(resultsdf[1:5000])
+#print(resultsdf)
 split_distances = pd.read_csv("split_distance.csv")
 resultsdf["dist"] = resultsdf["dist"].astype(str).str[:-1].astype(float)
 
 resultsdf.run_time = resultsdf.run_time.astype(float)
 resultsdf["track_id"] = resultsdf.apply(lambda s: track_id_gen(s["dist"],s["track"]), axis=1)
 resultsdf_merged = pd.merge(resultsdf, split_distances, on="track_id")
-form = resultsdf_merged
-race_forms = form.groupby(["race_id"])
-print(race_forms)
-race_input_layer = []
-race_classes = []
 
 resultsdf_merged["date"] = pd.to_datetime(resultsdf_merged["date"], format="%d %b %y")
 resultsdf_merged["speed"] = pd.to_numeric(resultsdf_merged["run_time"])/pd.to_numeric(resultsdf_merged["dist_x"])
@@ -21,7 +35,17 @@ resultsdf_merged["split_speed"] = pd.to_numeric(resultsdf_merged["split_times"])
 resultsdf_merged["box"] = pd.to_numeric(resultsdf_merged["box"])
 resultsdf_merged
 
-for i,j in tqdm.tqdm(race_forms):
+form = resultsdf_merged
+race_forms = form.groupby(["race_id"])
+print(race_forms)
+race_input_layer = []
+race_classes = []
+
+
+
+
+
+for i,j in race_forms:
     try:
         race_date = pd.to_datetime(j["date"].iloc[0])
         #print(f"{race_date=}")
@@ -32,9 +56,14 @@ for i,j in tqdm.tqdm(race_forms):
         i = 0
         for dog in dogs:
             #print(f"{dog=}")
-            prev_form = form[(form.dog_id == dog)&(form["date"]< race_date)]
+            prev_form = form[(form["date"]< race_date)]
+            prev_form = prev_form[(form.dog_id == dog)]
             dog_speed_avg = prev_form.speed.mean()
+            dog_speed_sd = prev_form.speed.std()
+            dog_speed_top = prev_form.speed.max()
             dog_split_avg = prev_form.split_speed.mean()
+            dog_split_sd = prev_form.split_speed.std()
+            dog_split_max = prev_form.split_speed.max()
             dog_race_count = prev_form.shape[0]
             dog_box = j.box.iloc[i]
             dog_stats = (dog, [dog_box,dog_speed_avg,dog_split_avg,dog_race_count], j.place.iloc[i])
